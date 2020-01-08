2dba57723e6462212e3f30fb14008183ad3e9556
Romario Gustave <romagustave10@gmail.com>
2020-01-08 21:33:18 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,7 +3,7 @@ require 'pry'
 class Artist
   attr_accessor :name
   attr_reader :songs
-  extend Memorable::ClassMethods
+  extend Memorable::ClassMethods 
   include Memorable::InstanceMethods
   extend Fidable 
   include Paramable
